a86a6d4bf6685fd8665b026d09eab38323e7cc97
James Dinh <87407302+jamesdinh12@users.noreply.github.com>
2024-09-27 14:36:05 +1000

    added more computations

--- avx2/avx2_simdParallelism.cpp
+++ avx2/avx2_simdParallelism.cpp
@@ -17,33 +17,44 @@ double m512d_sum(__m512d v) {
 }
 
 void perform_computations(double* a, double* b, double* c, int32_t* d, float* e) {
+    // AAD computations
     __m512d va = _mm512_load_pd(a);
     __m512d vb = _mm512_load_pd(b);
     __m512d vc = _mm512_load_pd(c);
-    __m256 ve = _mm256_load_ps(e);
-
-    __mmask8 mask = _mm512_cmpneq_pd_mask(va, _mm512_set1_pd(0.0));
-
+    __m512d zero = _mm512_set1_pd(0.0);
+    __mmask8 mask = _mm512_cmpneq_pd_mask(va, zero);
     __m512d vadd = _mm512_mask_add_pd(va, mask, va, vb);
     __m512d vfma = _mm512_mask3_fmadd_pd(va, vb, vc, mask);
+    _mm512_mask_store_pd(a, mask, vadd);
 
+    // Fast Exp computations
     __m512d vmax = _mm512_max_pd(va, vb);
     __m512d vmin = _mm512_min_pd(va, vb);
     __m512d vmul = _mm512_mul_pd(va, vb);
+    __m256 ve = _mm256_load_ps(e);
     __m512d vcvt = _mm512_cvt_roundps_pd(ve, _MM_FROUND_NO_EXC);
+    __m256i vd = _mm256_load_si256(reinterpret_cast<__m256i*>(d));
+    __m256 vef = _mm256_cvtepi32_ps(vd);
     __m256i vcvti = _mm512_cvt_roundpd_epi32(vc, _MM_FROUND_NO_EXC);
     __m512d vfmadd = _mm512_fmadd_pd(va, vb, vc);
     __m512d vsub = _mm512_sub_pd(va, vb);
     __m512d vcast = _mm512_castsi512_pd(_mm512_castpd_si512(vc));
     __m512i vcvt64 = _mm512_cvtpd_epi64(vc);
 
+    // State Fill computations
     double sum = m512d_sum(vc);
     __m512d vset = _mm512_set1_pd(sum);
+    __m512d vloadu = _mm512_loadu_pd(c);
 
     // Store results back to memory
-    _mm512_store_pd(a, vadd);
     _mm512_store_pd(b, vfma);
     _mm512_store_pd(c, vset);
+    _mm512_store_pd(a + 8, vmul);
+    _mm512_store_pd(b + 8, vsub);
+    _mm512_store_pd(c + 8, vfmadd);
+    _mm512_store_si512(reinterpret_cast<__m512i*>(d), vcvt64);
+    _mm256_store_si256(reinterpret_cast<__m256i*>(d + 16), vcvti);
+    _mm256_store_ps(e, vef);
 }
 
 int main() {
